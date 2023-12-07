2bce3fd876a635fa59b2c9eeac4947dec3a547e8
Justin Jongstra <00300061@rijnijssel.nl>
2023-12-07 15:14:27 +0100

    added mp4 sharing

--- notify_screenshot.sh
+++ notify_screenshot.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
 
-# Ensure environment variables are properly set (~/.bashrc or ~/.zshrc, etc.)
+# Ensure environment variables are properly set
 remote_server="${REMOTE_SERVER_FWSS}"
 remote_user="${REMOTE_USER_FWSS}"
 remote_path="${REMOTE_PATH_FWSS}"
 local_screenshot_dir="${LOCAL_SCREENSHOT_DIR_FWSS}"
+local_video_dir="${LOCAL_VIDEO_DIR_FWSS}"
 remote_url="${REMOTE_URL_FWSS}"
 
-# Ensure environment variables are properly set
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
-function copy_to_server() {
-    rsync -avz -e ssh "${local_screenshot_dir}/" "${remote_user}@${remote_server}:${remote_path}/"
+function sync_screenshots() {
+    if [ -n "$local_screenshot_dir" ]; then
+        inotifywait -m -e create --format '%f' "$local_screenshot_dir" |
+        while read filename; do
+            rsync -avz -e ssh "${local_screenshot_dir}/${filename}" "${remote_user}@${remote_server}:${remote_path}/images/"
+            echo "${remote_url}images/${filename}" | xclip -selection clipboard
+        done
+    else
+        echo "Local screenshot directory not set. Exiting..."
+        exit 1
+    fi
 }
 
-inotifywait -m -e create --format '%f' /home/"${USER}"/Pictures/Screenshots |
-while read filename; do
-    copy_to_server
-    url="${remote_url}${filename// /%20}"
-    echo "${url} copied to clipboard."
-    echo "${url}" | xclip -selection clipboard
-done
+function sync_videos() {
+    if [ -n "$local_video_dir" ]; then
+        inotifywait -m -e create --format '%f' "$local_video_dir" |
+        while read filename; do
+            rsync -avz -e ssh "${local_video_dir}/${filename}" "${remote_user}@${remote_server}:${remote_path}/videos/"
+            echo "${remote_url}video/${filename}" | xclip -selection clipboard
+        done
+    else
+        echo "Local video directory not set. Exiting..."
+        exit 1
+    fi
+}
+
+# Run functions only if the directories are set
+if [ -n "$local_screenshot_dir" ]; then
+    sync_screenshots &
+fi
+
+if [ -n "$local_video_dir" ]; then
+    sync_videos &
+fi
+
+# Keep the script running
+wait
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const PORT = 3003;
 
 app.use(express.static('public'));
 app.use('/images', express.static('images'));
+app.use('/videos', express.static('videos'));
 
 app.get('/image/:imageName', (req, res) => {
     const imageName = req.params.imageName;
@@ -26,6 +27,36 @@ app.get('/image/:imageName', (req, res) => {
       </head>
       <body>
         <img src="${imagePath}" alt="${imageName}" style="width:100%;height:auto;">
+      </body>
+    </html>
+  `;
+
+    res.send(htmlContent);
+});
+
+app.get('/video/:videoName', (req, res) => {
+    const videoName = req.params.videoName;
+    const videoPath = `/videos/${videoName}`;
+    const faviconPath = '../favicon.png';
+
+    // Construct HTML with image and metadata
+    const htmlContent = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <title>s.justinjongstra.nl | ${videoName}</title>
+        <link rel="icon" type="image/png" href="${faviconPath}">
+        <meta name="twitter:player" content="${videoPath}">
+        <meta name="twitter:card" content="player">
+        <meta name="twitter:title" content="Justin">
+        <meta name="twitter:description" content=" ">
+        <meta name="theme-color" content="#fa5560">
+      </head>
+      <body>
+        <video width="100%" height="auto" controls>
+          <source src="${videoPath}" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
       </body>
     </html>
   `;
